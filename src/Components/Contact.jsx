8f28f0583557b4b63acd21c60294a6106d46632a
j-belec <jeronimobelec@hotmail.com>
2023-07-04 12:07:03 -0300

    AboutUs section finished

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,7 +5,7 @@ import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
 
 function Contact() {
   return (
-    <section className={styles["section-contact"]}>
+    <section className={styles["section-contact"]} id="contact">
       <h2 className={styles["title"]}>Contacto</h2>
       <div className={styles["contact"]}>
         <form action="POST">
