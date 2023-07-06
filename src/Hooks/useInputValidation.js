3bfb4fce1c3ab5b49f6156765355247f43326ec6
j-belec <jeronimobelec@hotmail.com>
2023-07-06 15:41:40 -0300

    Contact validations finished

---
+++ src/Hooks/useInputValidation.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const useInputValidation = (validateValue) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+
+  const valueIsValid = validateValue(enteredValue);
+  const hasError = !valueIsValid && isTouched;
+
+  const valueChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+    // setIsTouched(false);
+  };
+
+  const inputBlurHandler = (event) => {
+    setEnteredValue(event.target.value);
+    setIsTouched(true);
+  };
+
+  const reset = () => {
+    setEnteredValue("");
+    setIsTouched(false);
+  };
+
+  return {
+    value: enteredValue,
+    isValid: valueIsValid,
+    hasError,
+    valueChangeHandler,
+    inputBlurHandler,
+    reset,
+  };
+};
+
+export default useInputValidation;
