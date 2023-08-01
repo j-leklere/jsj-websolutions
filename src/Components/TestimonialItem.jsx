a4b92a643df9513dc7bd209a97bdfbab69ecf7b9
Joaquin Leklere <jleklere@yahoo.com.ar>
2023-08-01 14:09:18 -0300

    Content updated

--- src/Components/TestimonialItem.jsx
+++ src/Components/TestimonialItem.jsx
@@ -3,17 +3,15 @@ import styles from "./TestimonialItem.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faQuoteRight } from "@fortawesome/free-solid-svg-icons";
 
-function TestimonialItem() {
+function TestimonialItem({ texto, nombre }) {
   return (
     <div className={styles["testimonial-item"]}>
       <FontAwesomeIcon icon={faQuoteRight} />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod
-        ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod tempor
-        incididunt ut labore et dolore magna aliqua.
-      </p>
-      <h3>Nombre Persona</h3>
-      <h4>ADEIP</h4>
+      <p>{texto}</p>
+      <div>
+        <h3>{nombre}</h3>
+        <h4>ADEIP</h4>
+      </div>
     </div>
   );
 }
